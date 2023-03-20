9d2e96297cc1c6bf5d83af45923c91e9a7ad6a63
Ahmed Zerrouk <ia_zerrouk@esi.dz>
2023-03-20 23:01:54 +0100

    Initial commit

---
+++ app/src/main/java/com/example/fooddeliveryapp/Auth/LogIn.kt
@@ -0,0 +1,12 @@
+package com.example.fooddeliveryapp.Auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fooddeliveryapp.R
+
+class LogIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
